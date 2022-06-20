6998a344e8726c073139c86e8ca3b527dc7f18bf
B. A. Ballesteros <104979664+AlfabetaWeb@users.noreply.github.com>
2022-06-20 03:21:02 +0200

    Connect to server and install Nodemon

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ app.use(express.json());   //te permite trabajar con json en este backend
 
 
 //Conexión a base de datos y levantar servidor
+// este callback llama al puerto para levantar el servidor y si falla envía un aviso
+db.then(()=>{
+
+    app.listen(port, ()=> {console.log("Servidor levantado en el puerto ", port)});
+
+}).catch((err) => console.log(err.message));
